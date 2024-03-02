55154d313bfec488da72525e3b3240a0bf86b772
MahmoudAlHaj4 <mahmoudalhaj509@gmail.com>
2024-03-02 14:24:25 +0200

    add dashes

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,16 @@
 const randomWords = ["helol", "bye", "Welcome"];
+const randomIndex = Math.floor(Math.random() * randomWords.length);
+const Word = randomWords[randomIndex];
+
+
+
+let dashes = document.getElementById("answer-section")
+ 
+
+let dashedWord = '';
+
+for (let i = 0; i < Word.length; i++) {
+
+    dashedWord += '-';
+}
+dashes.innerHTML = dashedWord;
